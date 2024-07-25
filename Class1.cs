bb81a9a26c7af84ba0caa44745eb0b0de2a05741
Govert van Drimmelen <govert@dnakode.com>
2024-07-26 00:21:51 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using ExcelDna.Documentation;
+using ExcelDna.Integration;
+
+namespace TestDnaDoc
+{
+    public class Class1
+    {
+        [ExcelFunctionDoc("Returns a greeting.", Description = "Function that returns a greeting.",
+            Returns = "The string 'Hello'.",
+            HelpTopic = "TestDnaDocOld-AddIn.chm!101")]
+        public static object SayHello(
+            [ExcelArgument(Name = "name", Description = "The name to greet.")] string name
+            )
+        {
+            return "Hello " + name;
+        }
+    }
+}
